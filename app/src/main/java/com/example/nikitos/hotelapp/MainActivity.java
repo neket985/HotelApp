c6e9e6708a71a2e4583935ab4645cca5856218a6
nikitos <neket98@bk.ru>
2016-05-23 20:17:08 +0300

    23.05.2016

--- app/src/main/java/com/example/nikitos/hotelapp/MainActivity.java
+++ app/src/main/java/com/example/nikitos/hotelapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.nikitos.hotelapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView tv;
     ImageView searchbtn;
+    EditText search_str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,22 +24,17 @@ public class MainActivity extends AppCompatActivity {
 
         tv = (TextView)findViewById(R.id.tv);
         searchbtn = (ImageView)findViewById(R.id.serchbtn);
+        search_str = (EditText)findViewById(R.id.search_string);
 
         View.OnClickListener oclSrchBtn = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String str =    getText(R.id.search_string).toString();
+                String str =   search_str.getText().toString();
                 tv.setText(str);
             }
         };
 
         searchbtn.setOnClickListener(oclSrchBtn);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
     }
 
 }
