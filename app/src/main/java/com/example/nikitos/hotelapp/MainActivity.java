d4b5feb8f1104919f976026b79aeb3743fedd769
nikitos <neket98@bk.ru>
2016-05-24 21:45:31 +0300

    24.05.2016

--- app/src/main/java/com/example/nikitos/hotelapp/MainActivity.java
+++ app/src/main/java/com/example/nikitos/hotelapp/MainActivity.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import android.text.SpannableString;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,21 +19,26 @@ public class MainActivity extends AppCompatActivity {
     TextView tv;
     ImageView searchbtn;
     EditText search_str;
+    Spinner spin_farang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         tv = (TextView)findViewById(R.id.tv);
         searchbtn = (ImageView)findViewById(R.id.serchbtn);
         search_str = (EditText)findViewById(R.id.search_string);
+        spin_farang = (Spinner)findViewById(R.id.spin_farang);
 
         View.OnClickListener oclSrchBtn = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String str =   search_str.getText().toString();
-                tv.setText(str);
+                String selected = spin_farang.getSelectedItem().toString();
+                tv.setText(selected+" "+str);
             }
         };
 
