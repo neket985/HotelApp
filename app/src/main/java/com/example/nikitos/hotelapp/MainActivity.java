4183a6a1c839283f3e52a7b588d6d6049dbfa573
nikitos <neket98@bk.ru>
2016-05-25 15:12:52 +0300

    25.05.2016

--- app/src/main/java/com/example/nikitos/hotelapp/MainActivity.java
+++ app/src/main/java/com/example/nikitos/hotelapp/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.nikitos.hotelapp;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.google.gson.*;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -28,36 +30,64 @@ public class MainActivity extends AppCompatActivity {
     ImageView searchbtn;
     EditText search_str;
     Spinner spin_farang;
+    Button sendForm;
 
     AutoCompleteTextView AutoCompleteCities;
-    String[] cities = { "Moscow", "France", "Germany", "Мурзик", "Рыжик", "Барсик", "Борис",
-            "Бегемот", "Мурка"};
-
+    String[] citiesWithid = { "Moscow :1", "France :2", "Germany :3"};
+    String[] cities;
+    String[] cityId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AutoCompleteCities = (AutoCompleteTextView) findViewById(R.id.Cities);
-        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, cities);
-        AutoCompleteCities.setAdapter(adapter1);
+        cities = new String[citiesWithid.length];
+        cityId = new String[citiesWithid.length];
+        for(int i=0;i<citiesWithid.length;++i){
+            cities[i] = citiesWithid[i].substring(0, citiesWithid[i].indexOf(':')-1);
+            cityId[i] = citiesWithid[i].substring(citiesWithid[i].indexOf(':')+1);
+        }
 
+        AutoCompleteCities = (AutoCompleteTextView) findViewById(R.id.Cities);
         tv = (TextView)findViewById(R.id.tv);
         searchbtn = (ImageView)findViewById(R.id.serchbtn);
         search_str = (EditText)findViewById(R.id.search_string);
         spin_farang = (Spinner)findViewById(R.id.spin_farang);
+        sendForm = (Button)findViewById(R.id.send_form);
 
-        View.OnClickListener oclSrchBtn = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String str =   search_str.getText().toString();
-                String selected = spin_farang.getSelectedItem().toString();
-                tv.setText(selected+" "+str);
-            }
-        };
 
-        searchbtn.setOnClickListener(oclSrchBtn);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, cities);
+        AutoCompleteCities.setAdapter(adapter1);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        final String[] searchReq = new String[1];
+        final String[] selectNumFarang = new String[1];
+        final String[] selectCity = new String[1];
+        final View.OnClickListener oclSrchImg = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchReq[0] = search_str.getText().toString();
+                tv.setText(searchReq[0]);
+            }
+        };
+        searchbtn.setOnClickListener(oclSrchImg);
+        View.OnClickListener oclFormBtn = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectCity[0] = AutoCompleteCities.getText().toString();
+                selectNumFarang[0] = spin_farang.getSelectedItem().toString();
+                tv.setText(selectCity[0]+" "+selectNumFarang[0]);
+            }
+        };
+        sendForm.setOnClickListener(oclFormBtn);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
 }
